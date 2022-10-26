483bbf28b3c92c80ea9c7a285dfc52fd4e3bd846
Allen K. Davis-Swing <akd111@ilab3.cs.rutgers.edu>
2022-10-26 09:31:43 -0400

    scheduler

--- code/benchmarks/test.c
+++ code/benchmarks/test.c
@@ -3,6 +3,13 @@
 #include <pthread.h>
 #include "../mypthread.h"
 
+void function() {
+	while (1) {
+		printf("printing stuff\n");
+		sleep(1);
+	}
+}
+
 /* A scratch program template on which to call and
  * test mypthread library functions as you implement
  * them.
@@ -15,8 +22,8 @@ int main(int argc, char **argv) {
 	/* Implement HERE */
 	mypthread_t test = 0;
   pthread_attr_t* attr = NULL;
-  mypthread_create(&test, attr, NULL, NULL);
-	mypthread_create(&test, attr, NULL, NULL);
+  mypthread_create(&test, attr, function, NULL);
+	mypthread_create(&test, attr, function, NULL);
 
 	print();
 	return 0;
